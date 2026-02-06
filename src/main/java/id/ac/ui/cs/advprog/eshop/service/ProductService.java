cde416f9c547ce26f8457f1c4513c3f03e8bb03e
unknown <hakimandi976@gmail.com>
2026-02-06 16:08:21 +0700

    Add edit product feature

--- src/main/java/id/ac/ui/cs/advprog/eshop/service/ProductService.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/service/ProductService.java
@@ -6,4 +6,7 @@ import java.util.List;
 public interface ProductService {
     public Product create(Product product);
     public List<Product> findAll();
+    Product findById(String id);   // ⬅️ INI
+    void update(Product product); // ⬅️ INI
+    void deleteById(String id);
 }
